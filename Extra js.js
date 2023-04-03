1f19a18bcd59d4eb1b3b54471b492430516bdd0e
anxumaan5x <47038767+anxumaan5x@users.noreply.github.com>
2023-04-03 19:11:30 +0530

    Create Extra js.js

---
+++ Extra js.js
@@ -0,0 +1,54 @@
+import React from "react";
+import Tree from "react-d3-tree";
+import orgChartJson from "./data/org-chart.json";
+import { useCenteredTree } from "./helpers";
+import "./styles.css";
+
+const containerStyles = {
+  width: "100vw",
+  height: "100vh"
+};
+
+// Here we're using `renderCustomNodeElement` render a component that uses
+// both SVG and HTML tags side-by-side.
+// This is made possible by `foreignObject`, which wraps the HTML tags to
+// allow for them to be injected into the SVG namespace.
+const renderForeignObjectNode = ({
+  nodeDatum,
+  toggleNode,
+  foreignObjectProps
+}) => (
+  <g>
+    <circle r={15}></circle>
+    {/* `foreignObject` requires width & height to be explicitly set. */}
+    <foreignObject {...foreignObjectProps}>
+      <div style={{ border: "1px solid black", backgroundColor: "#dedede" }}>
+        <h3 style={{ textAlign: "center" }}>{nodeDatum.name}</h3>
+        {nodeDatum.children && (
+          <button style={{ width: "100%" }} onClick={toggleNode}>
+            {nodeDatum.__rd3t.collapsed ? "Expand" : "Collapse"}
+          </button>
+        )}
+      </div>
+    </foreignObject>
+  </g>
+);
+
+export default function App() {
+  const [translate, containerRef] = useCenteredTree();
+  const nodeSize = { x: 200, y: 200 };
+  const foreignObjectProps = { width: nodeSize.x, height: nodeSize.y, x: 20 };
+  return (
+    <div style={containerStyles} ref={containerRef}>
+      <Tree
+        data={orgChartJson}
+        translate={translate}
+        nodeSize={nodeSize}
+        renderCustomNodeElement={(rd3tProps) =>
+          renderForeignObjectNode({ ...rd3tProps, foreignObjectProps })
+        }
+        orientation="vertical"
+      />
+    </div>
+  );
+}
